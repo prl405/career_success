d1d89dc317ffb83e4b14787d33f1ed6de24a0956
prl405 <73357564+prl405@users.noreply.github.com>
2025-03-03 12:52:23 +0000

    - Add request schema - Add WIP manual test notebook

---
+++ app/schemas/request.py
@@ -0,0 +1,13 @@
+
+class RequestModel:
+    age: int
+    gender: str
+    high_school_gpa: float
+    sat: int
+    uni_gpa: float
+    field: str
+    interships: int
+    projects: int
+    certifications: int
+    soft_skills: int
+    networking: int
